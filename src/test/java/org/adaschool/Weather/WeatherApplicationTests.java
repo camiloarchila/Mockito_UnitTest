81095afc75a95b5f6c10c2eaff5c8533023a3f66
camilo <camiloa@innfuturum.com>
2023-03-02 00:22:13 -0500

    mock test

--- src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
+++ src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
@@ -1,25 +1,36 @@
 package org.adaschool.Weather;
 
+import org.adaschool.Weather.controller.WeatherReportController;
+import org.adaschool.Weather.data.WeatherReport;
 import org.adaschool.Weather.service.WeatherReportService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
 class WeatherApplicationTests {
+
+	@InjectMocks
+	private WeatherReportService weatherReportService;
+
+	@Mock
+	private WeatherReport weatherReport;
 
 	@Test
 	void testWeatherReport() {
-		WeatherReportService weatherReportService = new WeatherReportService();
-		Assertions.assertEquals(86.0, weatherReportService.getWeatherReport(37.8267,-122.4233).getHumidity());
+		MockitoAnnotations.initMocks(this);
+		 WeatherReportController weatherReportController = mock(WeatherReportController.class);
+		weatherReportController.getWeatherReport(37.8267, -122.4233);
+		Assertions.assertEquals(weatherReport.getHumidity(), weatherReportService.getWeatherReport(37.8267,-122.4233).getHumidity());
 	}
 
 }
